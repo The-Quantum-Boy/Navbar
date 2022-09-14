63f365be01b793c16b3fcc042d76d1ffbfe5f33a
The-Quantum-Boy <dhanorkar.sumit.code@gmail.com>
2022-09-14 22:26:11 +0530

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Sidebar from './Sidebar'
 function App() {
+  
   return (
     <>
-      <h2>navbar project setup</h2>
+      <Navbar/>
+      
     </>
   )
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { links, social } from './data'
+
+
+const Sidebar = () => {
+    return (<>
+        <h4>sidebar</h4>
+        <div>
+
+            <div className="links-container show-container">
+                <ul className="links">
+                    {
+                        links.map((link) => {
+                            const { id, url, text } = link;
+                            return <li key={id}>
+                                <a href={url}>{text}</a>
+                            </li>
+                        })
+                    }
+                </ul>
+            </div>
+        </div>
+    </>
+    )
+}
+
+export default Sidebar;
